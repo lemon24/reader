985c962a45222e3d1daf28467e66dc1518dda918
Adrian Damian <damian.adrian24@gmail.com>
2018-02-11 01:52:16 +0200

    Add fakeparser.py (forgot to git add).

---
+++ tests/fakeparser.py
@@ -0,0 +1,61 @@
+from collections import OrderedDict
+
+from reader.types import Feed, Entry
+
+
+def _make_feed(number, updated):
+    return Feed(
+        'feed-{}.xml'.format(number),
+        'Feed #{}'.format(number),
+        'http://www.example.com/{}'.format(number),
+        updated,
+    )
+
+def _make_entry(number, updated, published=None):
+    return Entry(
+        'http://www.example.com/entries/{}'.format(number),
+        'Entry #{}'.format(number),
+        'http://www.example.com/entries/{}'.format(number),
+        updated,
+        published,
+        None,
+        None,
+        None,
+        False,
+    )
+
+class Parser:
+
+    def __init__(self, feeds=None, entries=None):
+        self.feeds = feeds or {}
+        self.entries = entries or {}
+
+    @classmethod
+    def from_parser(cls, other):
+        return cls(other.feeds, other.entries)
+
+    def feed(self, number, updated=None):
+        feed = _make_feed(number, updated)
+        self.feeds[number] = feed
+        self.entries.setdefault(number, OrderedDict())
+        return feed
+
+    def entry(self, feed_number, number, updated):
+        entry = _make_entry(number, updated)
+        self.entries[feed_number][number] = entry
+        return entry
+
+    def __call__(self, url, http_etag, http_last_modified):
+        for feed_number, feed in self.feeds.items():
+            if feed.url == url:
+                break
+        else:
+            raise RuntimeError("unkown feed: {}".format(url))
+        return feed, self.entries[feed_number].values(), http_etag, http_last_modified
+
+    def get_tuples(self):
+        for feed_number, entries in self.entries.items():
+            feed = self.feeds[feed_number]
+            for entry in entries.values():
+                yield feed, entry
+
