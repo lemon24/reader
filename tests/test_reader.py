678a615c3bd3c1ffc96ac7834901624fc9ef87ce
Adrian Damian <damian.adrian24@gmail.com>
2018-02-11 01:48:00 +0200

    Move performance-related tests to their own module.

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -15,19 +15,6 @@ def reader(monkeypatch, tmpdir):
     return Reader(':memory:')
 
 
-class BlockingParser(Parser):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.in_parser = threading.Event()
-        self.can_return_from_parser = threading.Event()
-
-    def __call__(self, *args, **kwargs):
-        self.in_parser.set()
-        self.can_return_from_parser.wait()
-        raise ParseError()
-
-
 def test_update_feed_updated(reader):
     """A feed should be processed only if it is newer than the stored one."""
 
@@ -124,43 +111,6 @@ def test_add_remove_feed(reader):
     assert set(reader.get_entries()) == set()
 
 
-@pytest.mark.slow
-def test_mark_as_read_during_update_feeds(monkeypatch, tmpdir):
-    monkeypatch.chdir(tmpdir)
-    db_path = str(tmpdir.join('db.sqlite'))
-
-    parser = Parser()
-    feed = parser.feed(1, datetime(2010, 1, 1))
-    entry = parser.entry(1, 1, datetime(2010, 1, 1))
-    feed2 = parser.feed(2, datetime(2010, 1, 1))
-
-    reader = Reader(db_path)
-    reader._parse = parser
-
-    reader.add_feed(feed.url)
-    reader.add_feed(feed2.url)
-    reader.update_feeds()
-
-    blocking_parser = BlockingParser.from_parser(parser)
-
-    def target():
-        reader = Reader(db_path)
-        reader._parse = blocking_parser
-        reader.update_feeds()
-
-    t = threading.Thread(target=target)
-    t.start()
-
-    blocking_parser.in_parser.wait()
-
-    try:
-        # shouldn't raise an exception
-        reader.mark_as_read(feed.url, entry.id)
-    finally:
-        blocking_parser.can_return_from_parser.set()
-        t.join()
-
-
 @pytest.mark.parametrize('chunk_size', [
     Reader._get_entries_chunk_size,     # the default
     1, 2, 3, 8,                         # rough result size for this test
@@ -207,42 +157,6 @@ def test_get_entries_order(reader, chunk_size):
         reverse=True)
 
     assert list(reader.get_entries()) == expected
-
-
-@pytest.mark.slow
-@pytest.mark.parametrize('chunk_size', [
-    Reader._get_entries_chunk_size,     # the default
-    1, 2, 3, 8,                         # rough result size for this test
-
-    # check unchunked queries still blocks writes
-    pytest.param(0, marks=pytest.mark.xfail(raises=Exception, strict=True)),
-])
-def test_mark_as_read_during_get_entries(monkeypatch, tmpdir, chunk_size):
-    monkeypatch.chdir(tmpdir)
-    db_path = str(tmpdir.join('db.sqlite'))
-
-    parser = Parser()
-    feed = parser.feed(1, datetime(2010, 1, 1))
-    entry = parser.entry(1, 1, datetime(2010, 1, 1))
-    parser.entry(1, 2, datetime(2010, 1, 2))
-    parser.entry(1, 3, datetime(2010, 1, 3))
-
-    reader = Reader(db_path)
-    reader._parse = parser
-    reader.add_feed(feed.url)
-    reader.update_feeds()
-
-    reader._get_entries_chunk_size = chunk_size
-
-    entries = reader.get_entries(_unread_only=True)
-    next(entries)
-
-    # shouldn't raise an exception
-    Reader(db_path).mark_as_read(feed.url, entry.id)
-    Reader(db_path).mark_as_unread(feed.url, entry.id)
-
-    # just a sanity check
-    assert len(list(entries)) == 3 - 1
 
 
 def test_get_feeds(reader):
