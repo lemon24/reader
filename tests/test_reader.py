ed7534efea49035a8ad5eab470f01366557cb84e
Adrian Damian <damian.adrian24@gmail.com>
2018-02-11 16:10:24 +0200

    Move "performance" tests back into API tests.

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -73,6 +73,59 @@ def test_update_entry_updated(reader, call_update_method):
     assert set(reader.get_entries()) == {(feed, new_entry)}
 
 
+class BlockingParser(Parser):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.in_parser = threading.Event()
+        self.can_return_from_parser = threading.Event()
+
+    def __call__(self, *args, **kwargs):
+        self.in_parser.set()
+        self.can_return_from_parser.wait()
+        raise ParseError()
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize('call_update_method', [call_update_feeds, call_update_feed])
+def test_update_blocking(monkeypatch, tmpdir, call_update_method):
+    """Calls to reader._parse() shouldn't block the underlying storage."""
+
+    monkeypatch.chdir(tmpdir)
+    db_path = str(tmpdir.join('db.sqlite'))
+
+    parser = Parser()
+    feed = parser.feed(1, datetime(2010, 1, 1))
+    entry = parser.entry(1, 1, datetime(2010, 1, 1))
+    feed2 = parser.feed(2, datetime(2010, 1, 1))
+
+    reader = Reader(db_path)
+    reader._parse = parser
+
+    reader.add_feed(feed.url)
+    reader.add_feed(feed2.url)
+    reader.update_feeds()
+
+    blocking_parser = BlockingParser.from_parser(parser)
+
+    def target():
+        reader = Reader(db_path)
+        reader._parse = blocking_parser
+        call_update_method(reader, feed.url)
+
+    t = threading.Thread(target=target)
+    t.start()
+
+    blocking_parser.in_parser.wait()
+
+    try:
+        # shouldn't raise an exception
+        reader.mark_as_read(feed.url, entry.id)
+    finally:
+        blocking_parser.can_return_from_parser.set()
+        t.join()
+
+
 def test_mark_as_read_unread(reader):
 
     parser = Parser()
@@ -194,6 +247,44 @@ def test_get_entries_feed_url(reader):
     assert set(reader.get_entries(feed_url='bad feed')) == set()
 
     # TODO: How do we test the combination between which and feed_url?
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize('chunk_size', [
+    Reader._get_entries_chunk_size,     # the default
+    1, 2, 3, 8,                         # rough result size for this test
+
+    # check unchunked queries still blocks writes
+    pytest.param(0, marks=pytest.mark.xfail(raises=Exception, strict=True)),
+])
+def test_get_entries_blocking(monkeypatch, tmpdir, chunk_size):
+    """Unconsumed reader.get_entries() shouldn't block the underlying storage."""
+
+    monkeypatch.chdir(tmpdir)
+    db_path = str(tmpdir.join('db.sqlite'))
+
+    parser = Parser()
+    feed = parser.feed(1, datetime(2010, 1, 1))
+    entry = parser.entry(1, 1, datetime(2010, 1, 1))
+    parser.entry(1, 2, datetime(2010, 1, 2))
+    parser.entry(1, 3, datetime(2010, 1, 3))
+
+    reader = Reader(db_path)
+    reader._parse = parser
+    reader.add_feed(feed.url)
+    reader.update_feeds()
+
+    reader._get_entries_chunk_size = chunk_size
+
+    entries = reader.get_entries(which='unread')
+    next(entries)
+
+    # shouldn't raise an exception
+    Reader(db_path).mark_as_read(feed.url, entry.id)
+    Reader(db_path).mark_as_unread(feed.url, entry.id)
+
+    # just a sanity check
+    assert len(list(entries)) == 3 - 1
 
 
 def test_add_remove_get_feeds(reader):
