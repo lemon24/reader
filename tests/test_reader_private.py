00bff01ee90875dcff941daa69cb7cc52a3afcb0
Adrian Damian <damian.adrian24@gmail.com>
2018-02-11 21:17:08 +0200

    Add test for reader._parse() internal interface.

--- tests/test_reader_private.py
+++ tests/test_reader_private.py
@@ -27,21 +27,39 @@ def test_update_stale(reader, call_update_method):
     reader.add_feed(feed.url)
 
     call_update_method(reader, feed.url)
-    assert parser.calls == [(feed.url, None, None)]
     assert set(reader.get_entries()) == {(feed, entry)}
-    parser.calls[:] = []
 
     new_feed = parser.feed(1, datetime(2010, 1, 1), title="new feed title")
     new_entry = parser.entry(1, 1, datetime(2010, 1, 1), title="new entry title")
 
     call_update_method(reader, feed.url)
-    assert parser.calls == [(feed.url, 'etag', 'last-modified')]
     assert set(reader.get_entries()) == {(feed, entry)}
-    parser.calls[:] = []
 
+    parser.calls[:] = []
     reader._mark_as_stale(feed.url)
     call_update_method(reader, feed.url)
     assert parser.calls == [(feed.url, None, None)]
     assert set(reader.get_entries()) == {(new_feed, new_entry)}
+
+
+@pytest.mark.parametrize('call_update_method', [call_update_feeds, call_update_feed])
+def test_update_parse(reader, call_update_method):
+    """Updated feeds should pass caching headers back to ._parse()."""
+
+    parser = ParserThatRemembers()
+    parser.http_etag = 'etag'
+    parser.http_last_modified = 'last-modified'
+    reader._parse = parser
+
+    feed = parser.feed(1, datetime(2010, 1, 1))
+    entry = parser.entry(1, 1, datetime(2010, 1, 1))
+
+    reader.add_feed(feed.url)
+
+    call_update_method(reader, feed.url)
+    assert parser.calls == [(feed.url, None, None)]
+
     parser.calls[:] = []
+    call_update_method(reader, feed.url)
+    assert parser.calls == [(feed.url, 'etag', 'last-modified')]
 
