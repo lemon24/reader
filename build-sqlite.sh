a38d20ea83684bc87b64a7711c753d0b331490a1
Adrian Damian <damian.adrian24@gmail.com>
2018-02-22 00:06:46 +0200

    Use newer SQLite version with Travis.

---
+++ build-sqlite.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ ! -d $HOME/sqlite ]; then
+    echo "compiling sqlite"
+    wget https://sqlite.org/2018/sqlite-amalgamation-3220000.zip
+    unzip sqlite-amalgamation-3220000.zip
+    rm sqlite-amalgamation-3220000.zip
+    mv sqlite-amalgamation-3220000 sqlite
+    cd sqlite
+    gcc -c -fPIC -O2 -I. \
+        -DSQLITE_THREADSAFE=0 \
+        -DSQLITE_ENABLE_FTS4 \
+        -DSQLITE_ENABLE_FTS5 \
+        -DSQLITE_ENABLE_JSON1 \
+        -DSQLITE_ENABLE_RTREE \
+        -DSQLITE_ENABLE_EXPLAIN_COMMENTS \
+        -DHAVE_USLEEP \
+        sqlite3.c
+    gcc -shared -o libsqlite3.so -fPIC sqlite3.o -ldl -lpthread
+    ln -s libsqlite3.so libsqlite3.so.0
+    cd ..
+    mv sqlite $HOME
+else
+    echo "using existing sqlite"
+fi
+
