397d0465a01f730cd6e98df4b472e4eab1069468
Adrian Damian <damian.adrian24@gmail.com>
2018-02-25 23:25:15 +0200

    Add mark-all-as-read test.
    Part of #30.

--- tests/test_app.py
+++ tests/test_app.py
@@ -53,6 +53,49 @@ def test_mark_as_read_unread(tmpdir):
     assert len(browser.get_current_page().select('form.entry')) == 1
 
 
+@pytest.mark.slow
+def test_mark_all_as_read_unread(tmpdir):
+    db_path = str(tmpdir.join('db.sqlite'))
+
+    parser = Parser()
+    feed = parser.feed(1, datetime(2010, 1, 1))
+    entry = parser.entry(1, 1, datetime(2010, 1, 1))
+
+    reader = Reader(db_path)
+    reader._parse = parser
+
+    reader.add_feed(feed.url)
+    reader.update_feeds()
+
+    app = create_app(db_path)
+
+    session = requests.Session()
+    session.mount('http://app/', wsgiadapter.WSGIAdapter(app))
+    browser = mechanicalsoup.StatefulBrowser(session)
+    browser.open('http://app/', params={'feed': feed.url})
+
+    assert len(browser.get_current_page().select('form.entry')) == 1
+
+    form = browser.select_form('form#update-entries')
+    form.set_checkbox({'really': True})
+    response = browser.submit_selected(form.form.find('button', text='mark all as read'))
+    assert response.status_code == 200
+    assert len(browser.get_current_page().select('form.entry')) == 0
+
+    response = browser.follow_link(browser.find_link(text='read'))
+    assert response.status_code == 200
+    assert len(browser.get_current_page().select('form.entry')) == 1
+
+    form = browser.select_form('form#update-entries')
+    form.set_checkbox({'really': True})
+    response = browser.submit_selected(form.form.find('button', text='mark all as unread'))
+    assert response.status_code == 200
+    assert len(browser.get_current_page().select('form.entry')) == 0
+
+    response = browser.follow_link(browser.find_link(text='unread'))
+    assert response.status_code == 200
+    assert len(browser.get_current_page().select('form.entry')) == 1
+
 
 @pytest.mark.slow
 def test_add_delete_feed(tmpdir):
