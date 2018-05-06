88b5ad2a9b05871892cc6a5bbc1fba5b06b29a6a
Adrian Damian <damian.adrian24@gmail.com>
2018-05-07 00:52:34 +0300

    Add feed and entry attribute docstrings.
    Part of #28.

--- reader/types.py
+++ reader/types.py
@@ -30,23 +30,51 @@ class attrs_namedtuple_compat:
 
 @attr.s(slots=True, frozen=True)
 class Feed(attrs_namedtuple_compat):
+
+    #: The URL of the feed.
     url = attr.ib()
+
+    #: The date the feed was last updated.
     updated = attr.ib(default=None)
+
+    #: The title of the feed.
     title = attr.ib(default=None)
+
+    #: The URL of a page associated with the feed.
     link = attr.ib(default=None)
+
+    #: User-defined feed title.
     user_title = attr.ib(default=None)
 
 
 @attr.s(slots=True, frozen=True)
 class Entry(attrs_namedtuple_compat):
+
+    #: Entry identifier.
     id = attr.ib()
+
+    #: The date the entry was last updated.
     updated = attr.ib()
+
+    #: The title of the entry.
     title = attr.ib(default=None)
+
+    #: The URL of a page associated with the entry.
     link = attr.ib(default=None)
+
+    #: The date the entry was first published.
     published = attr.ib(default=None)
+
+    #: A summary of the entry.
     summary = attr.ib(default=None)
+
+    #: Entry content. An iterable of :class:`Content` objects.
     content = attr.ib(default=None)
+
+    #: Entry enclosures. An iterable of :class:`Enclosure` objects.
     enclosures = attr.ib(default=None)
+
+    #: Whether the entry was read or not.
     read = attr.ib(default=False)
 
 
