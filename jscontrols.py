1af1a73bb8a208520e21cadd86bfaea030f61c8c
Adrian Damian <damian.adrian24@gmail.com>
2018-05-23 23:16:19 +0300

    Move APIThing back to app.py.

--- jscontrols.py
+++ jscontrols.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, flash, jsonify
 import werkzeug
 
-from reader.app import get_flashed_messages_by_prefix, redirect_to_referrer, is_safe_url
+from reader.app import get_flashed_messages_by_prefix, APIThing, APIError
 
 
 app = Flask(
@@ -88,91 +88,6 @@ window.onload = function () {
 """).render()
 
 
-class APIThing:
-
-    def __init__(self, blueprint, rule, endpoint):
-        self.actions = {}
-        self.really = {}
-        blueprint.add_url_rule(rule, endpoint, methods=['POST'], view_func=self.dispatch)
-
-    def dispatch_form(self):
-        action = request.form['action']
-        func = self.actions.get(action)
-        if func is None:
-            return "unknown action", 400
-        next = request.form.get('next-' + action)
-        if next is None:
-            next = request.form['next']
-        if not is_safe_url(next):
-            return "bad next", 400
-        if self.really[func]:
-            really = request.form.get('really-' + action)
-            if really is None:
-                really = request.form.get('really')
-            target = request.form.get('target')
-            if really != 'really':
-                category = (action, )
-                if target is not None:
-                    category += (target, )
-                flash("{}: really not checked".format(action), category)
-                return redirect_to_referrer()
-        try:
-            rv = func(request.form)
-            flash(rv)
-        except APIError as e:
-            category = (action, )
-            if e.category:
-                category += e.category
-            flash("{}: {}".format(action, e), category)
-            return redirect_to_referrer()
-        return redirect(next)
-
-    def dispatch_json(self):
-        data = werkzeug.MultiDict(request.get_json())
-        action = data['action']
-        func = self.actions.get(action)
-        if func is None:
-            return "unknown action", 400
-
-        try:
-            rv = func(data)
-            rv = {'ok': rv}
-        except APIError as e:
-            category = (action, )
-            if e.category:
-                category += e.category
-            rv = {'err': e.message}
-
-        return jsonify(rv)
-
-    def dispatch(self):
-        if request.mimetype == 'application/x-www-form-urlencoded':
-            return self.dispatch_form()
-        if request.mimetype == 'application/json':
-            return self.dispatch_json()
-        return "bad content type", 400
-
-    def __call__(self, func=None, *, really=False):
-
-        def register(f):
-            self.actions[f.__name__.replace('_', '-')] = f
-            self.really[f] = really
-            return f
-
-        if func is None:
-            return register
-        return register(func)
-
-
-class APIError(Exception):
-
-    def __init__(self, message, category=None):
-        super().__init__(message)
-        self.message = message
-        if category is not None:
-            if not isinstance(category, tuple):
-                category = (category, )
-        self.category = category
 
 
 
