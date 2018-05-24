f27ea12c2c685b9f7388f6ff296400453cb37745
Adrian Damian <damian.adrian24@gmail.com>
2018-05-24 22:19:20 +0300

    Expose --host and --port options on the serve command.

--- reader/cli.py
+++ reader/cli.py
@@ -96,13 +96,15 @@ def update(db_path, url, verbose):
 
 @cli.command()
 @click.pass_obj
+@click.option('-h', '--host', default='localhost', show_default=True)
+@click.option('-p', '--port', default=8080, show_default=True, type=int)
 @click.option('-v', '--verbose', count=True)
-def serve(db_path, verbose):
+def serve(db_path, host, port, verbose):
     setup_logging(verbose)
     from werkzeug.serving import run_simple
     from .app import create_app
     app = create_app(db_path)
-    run_simple('localhost', 8080, app)
+    run_simple(host, port, app)
 
 
 
