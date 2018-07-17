7ead3dfa43554895562da9881248929cc1e3cfec
Adrian Damian <damian.adrian24@gmail.com>
2018-07-17 22:14:36 +0300

    Move the feedparser parse() snippet into its own module.
    Part of #67.

--- reader/parser.py
+++ reader/parser.py
@@ -125,110 +125,18 @@ def _process_feed(url, d):
 
 
 try:
-    import urllib.parse
-    import xml.sax
     import requests
 except ImportError:
     requests = None
 
 if requests:
+    from ._feedparser_parse_data import _feedparser_parse_snippet
     try:
         import feedparser.http as feedparser_http
         import feedparser.api as feedparser_api
     except ImportError:
         feedparser_http = feedparser
         feedparser_api = feedparser
-
-
-def _feedparser_parse_snippet(result, data, resolve_relative_uris=True, sanitize_html=True):
-    """This is a verbatim snippet from feedparser.api.parse().
-
-    https://github.com/kurtmckee/feedparser/blob/5646f4ca2069ffea349618eef9566005afec665e/feedparser/api.py#L168
-
-    """
-    try:
-        # for the GitHub version
-        from feedparser.api import (
-            convert_to_utf8,
-            StrictFeedParser,
-            LooseFeedParser,
-            bytes_,
-            replace_doctype, _makeSafeAbsoluteURI,
-            _XML_AVAILABLE, _SGML_AVAILABLE, PREFERRED_XML_PARSERS,
-            _StringIO,
-        )
-    except ImportError:
-        # for '5.2.1'
-        from feedparser import (
-            convert_to_utf8 as original_convert_to_utf8,
-            _StrictFeedParser as StrictFeedParser,
-            _LooseFeedParser as LooseFeedParser,
-            replace_doctype, _makeSafeAbsoluteURI,
-            _XML_AVAILABLE, _SGML_AVAILABLE, PREFERRED_XML_PARSERS,
-            _StringIO,
-        )
-
-        def convert_to_utf8(http_headers, data, result):
-            data, rfc3023_encoding, error = original_convert_to_utf8(http_headers, data)
-            result['encoding'] = rfc3023_encoding
-            if error:
-                result['bozo'] = 1
-                result['bozo_exception'] = error
-            return data
-
-        bytes_ = bytes
-
-    # BEGIN "feedparser.api.parse()"
-
-    data = convert_to_utf8(result['headers'], data, result)
-    use_strict_parser = result['encoding'] and True or False
-
-    result['version'], data, entities = replace_doctype(data)
-
-    # Ensure that baseuri is an absolute URI using an acceptable URI scheme.
-    contentloc = result['headers'].get('content-location', '')
-    href = result.get('href', '')
-    baseuri = _makeSafeAbsoluteURI(href, contentloc) or _makeSafeAbsoluteURI(contentloc) or href
-
-    baselang = result['headers'].get('content-language', None)
-    if isinstance(baselang, bytes_) and baselang is not None:
-        baselang = baselang.decode('utf-8', 'ignore')
-
-    if not _XML_AVAILABLE:
-        use_strict_parser = 0
-    if use_strict_parser:
-        # initialize the SAX parser
-        feedparser = StrictFeedParser(baseuri, baselang, 'utf-8')
-        feedparser.resolve_relative_uris = resolve_relative_uris
-        feedparser.sanitize_html = sanitize_html
-        saxparser = xml.sax.make_parser(PREFERRED_XML_PARSERS)
-        saxparser.setFeature(xml.sax.handler.feature_namespaces, 1)
-        try:
-            # disable downloading external doctype references, if possible
-            saxparser.setFeature(xml.sax.handler.feature_external_ges, 0)
-        except xml.sax.SAXNotSupportedException:
-            pass
-        saxparser.setContentHandler(feedparser)
-        saxparser.setErrorHandler(feedparser)
-        source = xml.sax.xmlreader.InputSource()
-        source.setByteStream(_StringIO(data))
-        try:
-            saxparser.parse(source)
-        except xml.sax.SAXException as e:
-            result['bozo'] = 1
-            result['bozo_exception'] = feedparser.exc or e
-            use_strict_parser = 0
-    if not use_strict_parser and _SGML_AVAILABLE:
-        feedparser = LooseFeedParser(baseuri, baselang, 'utf-8', entities)
-        feedparser.resolve_relative_uris = resolve_relative_uris
-        feedparser.sanitize_html = sanitize_html
-        feedparser.feed(data.decode('utf-8', 'replace'))
-    result['feed'] = feedparser.feeddata
-    result['entries'] = feedparser.entries
-    result['version'] = result['version'] or feedparser.version
-    result['namespaces'] = feedparser.namespacesInUse
-
-    # END "feedparser.api.parse()"
 
 
 def parse_requests(url, http_etag=None, http_last_modified=None):
