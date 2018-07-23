5764e72b5fb92f3eef8b8726f38fe7fdbaa44fd8
Adrian Damian <damian.adrian24@gmail.com>
2018-07-24 01:54:25 +0300

    Add RequestsParser Tumblr GDPR plugin.
    Part of #67.

--- reader/parser.py
+++ reader/parser.py
@@ -227,9 +227,60 @@ class RequestsParser:
         return _process_feed(url, result) + (http_etag, http_last_modified)
 
 
+def tumblr_gdpr_plugin(session, response, request):
+    """Accept Tumblr GDPR  stuff.
+
+    https://github.com/lemon24/reader/issues/67
+
+    """
+    if not response.url.startswith('https://www.tumblr.com/privacy/consent'):
+        return None
+
+    CONSENT_FORM_SVC = 'https://www.tumblr.com/svc/privacy/consent'
+
+    def make_headers(consent_form_url, tumblr_form_key):
+        return {
+            # anything starting with 'https://www.tumblr.com/' should work
+            'Referer': consent_form_url,
+            'X-tumblr-form-key': tumblr_form_key,
+        }
+
+    def make_json_data():
+        return {
+            'eu_resident': True,
+            'gdpr_consent_core': True,
+            'gdpr_consent_first_party_ads': True,
+            'gdpr_consent_search_history': True,
+            'gdpr_consent_third_party_ads': True,
+            'gdpr_is_acceptable_age': True,
+        }
+
+    def extract_tumblr_form_key(text):
+        import re
+        match = re.search('<meta name="tumblr-form-key" id="tumblr_form_key" content="([^"]+)">', text)
+        assert match
+        return match.group(1)
+
+    def fill_cookie_jar_requests(session, consent_form_url):
+        response = session.get(consent_form_url)
+        assert response.status_code == 200
+
+        tumblr_form_key = extract_tumblr_form_key(response.text)
+
+        headers = make_headers(consent_form_url, tumblr_form_key)
+        json_data = make_json_data()
+
+        response = session.post(CONSENT_FORM_SVC, json=json_data, headers=headers)
+        assert response.status_code == 200
+
+    fill_cookie_jar_requests(session, response.url)
+    return request
+
+
 parse = parse_feedparser
 if requests:
     parse_requests = RequestsParser()
     parse_requests.__name__ = parse_requests.__qualname__ = 'parse_requests'
+    parse_requests.response_plugins.append(tumblr_gdpr_plugin)
     parse = parse_requests
 
