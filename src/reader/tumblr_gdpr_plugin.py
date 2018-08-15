1eecd577cc06e8fd318e5ae67dee8b2c99b4bbc7
Adrian Damian <damian.adrian24@gmail.com>
2018-08-15 18:47:08 +0300

    Move tumblr_gdpr_plugin.py to its own module.
    Part of #83.

---
+++ src/reader/tumblr_gdpr_plugin.py
@@ -0,0 +1,51 @@
+import re
+
+
+def make_headers(consent_form_url, tumblr_form_key):
+    return {
+        # anything starting with 'https://www.tumblr.com/' should work
+        'Referer': consent_form_url,
+        'X-tumblr-form-key': tumblr_form_key,
+    }
+
+def make_json_data():
+    return {
+        'eu_resident': True,
+        'gdpr_consent_core': True,
+        'gdpr_consent_first_party_ads': True,
+        'gdpr_consent_search_history': True,
+        'gdpr_consent_third_party_ads': True,
+        'gdpr_is_acceptable_age': True,
+    }
+
+def extract_tumblr_form_key(text):
+    match = re.search('<meta name="tumblr-form-key" id="tumblr_form_key" content="([^"]+)">', text)
+    assert match
+    return match.group(1)
+
+def fill_cookie_jar_requests(session, consent_form_url):
+    response = session.get(consent_form_url)
+    assert response.status_code == 200
+
+    tumblr_form_key = extract_tumblr_form_key(response.text)
+    headers = make_headers(consent_form_url, tumblr_form_key)
+    json_data = make_json_data()
+
+    response = session.post('https://www.tumblr.com/svc/privacy/consent',
+                            json=json_data, headers=headers)
+    assert response.status_code == 200
+
+
+def tumblr_gdpr_plugin(session, response, request):
+    """Accept Tumblr GDPR  stuff.
+
+    https://github.com/lemon24/reader/issues/67
+
+    """
+    if not response.url.startswith('https://www.tumblr.com/privacy/consent'):
+        return None
+
+    fill_cookie_jar_requests(session, response.url)
+    return request
+
+
