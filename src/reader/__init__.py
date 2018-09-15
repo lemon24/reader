47ffd852aea80d9abcfdf2cc9ded183c59ecb5ca
Adrian Damian <damian.adrian24@gmail.com>
2018-09-15 15:32:32 +0300

    Add package docstring with usage example.
    For #61.

--- src/reader/__init__.py
+++ src/reader/__init__.py
@@ -1,3 +1,41 @@
+"""
+reader
+======
+
+A minimal feed reader.
+
+Usage
+-----
+
+Here is small example of using reader.
+
+Create a Reader object::
+
+    reader = Reader('db.sqlite')
+
+Add a feed::
+
+    reader.add_feed('http://www.hellointernet.fm/podcast?format=rss')
+
+Update all the feeds::
+
+    reader.update_feeds()
+
+Get all the entries, both read and unread::
+
+    entries = list(reader.get_entries())
+
+Mark the first entry as read::
+
+    reader.mark_as_read(entries[0])
+
+Print the titles of the unread entries::
+
+    for e in reader.get_entries(which='unread'):
+        print(e.title)
+
+
+"""
 
 __version__ = '0.1.dev0'
 
