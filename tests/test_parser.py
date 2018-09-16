e1e1ef973d1049cce172a4fec678eacb53175a46
Adrian Damian <damian.adrian24@gmail.com>
2018-09-16 15:25:23 +0300

    Use requests-mock in test_parser.py instead of a web server.
    For #90.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -22,50 +22,77 @@ def parse():
 
 
 @pytest.fixture
-def make_relative_path_url(tmpdir):
+def make_relative_path_url(**_):
     yield lambda feed_path: feed_path.basename
 
 @pytest.fixture
-def make_absolute_path_url(tmpdir):
+def make_absolute_path_url(**_):
     yield lambda feed_path: str(feed_path)
 
 @pytest.fixture
-def make_http_url(tmpdir):
-    with run_httpd() as httpd:
-        yield lambda feed_path: httpd.url + feed_path.basename
+def make_http_url(requests_mock, **_):
+    def make_url(feed_path):
+        url = 'http://example.com/' + feed_path.basename
+        headers = {}
+        if feed_path.ext == '.rss':
+            headers['Content-Type'] = 'application/x-rss+xml'
+        elif feed_path.ext == '.atom':
+            headers['Content-Type'] = 'application/atom+xml'
+        requests_mock.get(url, text=feed_path.read(), headers=headers)
+        return url
+    yield make_url
 
 @pytest.fixture
-def make_https_url(tmpdir):
-    certfile = str(tmpdir.join('server.pem'))
-    subprocess.run(
-        ['openssl', 'req', '-new', '-x509', '-keyout', certfile,
-         '-out', certfile, '-days', '365', '-nodes'], input=b'\n'*7)
-    with run_httpd(certfile=certfile) as httpd:
-        yield lambda feed_path: httpd.url + feed_path.basename
+def make_https_url(requests_mock, **_):
+    def make_url(feed_path):
+        url = 'http://example.com/' + feed_path.basename
+        headers = {}
+        if feed_path.ext == '.rss':
+            headers['Content-Type'] = 'application/x-rss+xml'
+        elif feed_path.ext == '.atom':
+            headers['Content-Type'] = 'application/atom+xml'
+        requests_mock.get(url, text=feed_path.read(), headers=headers)
+        return url
+    yield make_url
 
 @pytest.fixture
-def make_http_gzip_url(tmpdir):
-    with run_httpd(gzip=True) as httpd:
-        yield lambda feed_path: httpd.url + feed_path.basename
+def make_http_gzip_url(requests_mock, **_):
+    def make_url(feed_path):
+        url = 'http://example.com/' + feed_path.basename
+        headers = {}
+        if feed_path.ext == '.rss':
+            headers['Content-Type'] = 'application/x-rss+xml'
+        elif feed_path.ext == '.atom':
+            headers['Content-Type'] = 'application/atom+xml'
+        headers['Content-Encoding'] = 'gzip'
 
+        import io, gzip
+        compressed_file = io.BytesIO()
+        gz = gzip.GzipFile(fileobj=compressed_file, mode='wb')
+        gz.write(feed_path.read_binary())
+        gz.close()
+
+        requests_mock.get(url, content=compressed_file.getvalue(), headers=headers)
+        return url
+    yield make_url
 
 @pytest.fixture(params=[
     make_relative_path_url,
     make_absolute_path_url,
-    pytest.param(make_http_url, marks=pytest.mark.slow),
-    pytest.param(make_https_url, marks=pytest.mark.slow),
-    pytest.param(make_http_gzip_url, marks=pytest.mark.slow),
+    make_http_url,
+    make_https_url,
+    make_http_gzip_url,
 ])
-def make_url(request, tmpdir):
-    yield from request.param(tmpdir)
+def make_url(request, requests_mock):
+    yield from request.param(requests_mock=requests_mock)
 
 
 @pytest.fixture(params=[
     make_relative_path_url,
     pytest.param(make_http_url, marks=pytest.mark.slow),
 ])
-def make_url_local_remote(request, tmpdir):
-    yield from request.param(tmpdir)
+def make_url_local_remote(request, requests_mock):
+    yield from request.param(requests_mock=requests_mock)
 
 
 @pytest.mark.parametrize('feed_type', ['rss', 'atom'])
@@ -160,11 +187,13 @@ def test_parse_character_encoding_override(monkeypatch, parse, data_dir):
 
 
 @pytest.fixture
-def make_http_url_304():
-    with run_httpd(error_code=304) as httpd:
-        yield lambda feed_path: httpd.url + feed_path.basename
+def make_http_url_304(requests_mock):
+    def make_url(feed_path):
+        url = 'http://example.com/' + feed_path.basename
+        requests_mock.get(url, status_code=304)
+        return url
+    yield make_url
 
-@pytest.mark.slow
 def test_parse_not_modified(monkeypatch, parse, make_http_url_304, data_dir):
     """parse() should raise NotModified for unchanged feeds."""
 
@@ -177,21 +206,28 @@ def test_parse_not_modified(monkeypatch, parse, make_http_url_304, data_dir):
 
 
 @pytest.fixture
-def make_http_get_headers_url():
-    with run_httpd(request_headers=True) as httpd:
-        def make_url(feed_path):
-            return httpd.url + feed_path.basename
-        make_url.httpd = httpd
-        yield make_url
+def make_http_get_headers_url(requests_mock):
+    def make_url(feed_path):
+        url = 'http://example.com/' + feed_path.basename
+        headers = {}
+        if feed_path.ext == '.rss':
+            headers['Content-Type'] = 'application/x-rss+xml'
+        elif feed_path.ext == '.atom':
+            headers['Content-Type'] = 'application/atom+xml'
+        def callback(request, context):
+            make_url.request_headers = request.headers
+            return feed_path.read()
+        requests_mock.get(url, text=callback, headers=headers)
+        return url
+    yield make_url
 
-@pytest.mark.slow
 def test_parse_sends_etag_last_modified(monkeypatch, parse, make_http_get_headers_url, data_dir):
     monkeypatch.chdir(data_dir)
 
     feed_url = make_http_get_headers_url(data_dir.join('full.atom'))
     parse(feed_url, 'etag', 'last_modified')
 
-    headers = make_http_get_headers_url.httpd.request_headers
+    headers = make_http_get_headers_url.request_headers
 
     assert headers.get('If-None-Match') == 'etag'
     assert headers.get('If-Modified-Since') == 'last_modified'
@@ -214,11 +250,11 @@ def test_parse_local_timezone(monkeypatch, request, parse, tz, data_dir):
     assert feed.updated == expected['feed'].updated
 
 
-@pytest.mark.slow
 def test_parse_response_plugins(monkeypatch, tmpdir, make_http_url, data_dir):
     monkeypatch.chdir(data_dir)
 
     feed_url = make_http_url(data_dir.join('empty.atom'))
+    make_http_url(data_dir.join('full.atom'))
 
     import requests
 
