9bb25cddb3fa0ea1842b88e0b31671e00daef5e4
Adrian Damian <damian.adrian24@gmail.com>
2018-12-22 22:57:44 +0200

    Add feed_entry_dedupe plugin.
    For #79.

---
+++ src/reader/plugins/feed_entry_dedupe.py
@@ -0,0 +1,89 @@
+"""
+Deduplicate entries for the same feed.
+
+Duplicates are entries with the same title *and* summary/content.
+
+If the old entry is read, the new one will be too.
+If the old entry is unread, it will be marked as read in favor of the new one.
+
+To load:
+
+    READER_DB=db.sqlite \
+    READER_PLUGIN='reader.plugins.feed_entry_dedupe:feed_entry_dedupe' \
+    python -m reader update -v
+
+Implemented for https://github.com/lemon24/reader/issues/79
+
+TODO: Some possible optimizations:
+
+1. Do this once per feed (now it's one get_entries(feed=...) per entry).
+2. Only get entries with the same title (not possible with the current API).
+
+
+"""
+
+import re
+import logging
+
+log = logging.getLogger('reader.plugins.feed_entry_dedupe')
+
+
+XML_TAG_RE = re.compile(r'<[^<]+?>', re.I)
+XML_ENTITY_RE = re.compile(r'&[^\s;]+?;', re.I)
+WHITESPACE_RE = re.compile(r'\s+')
+
+def normalize(text):
+    text = XML_TAG_RE.sub(' ', text)
+    text = XML_ENTITY_RE.sub(' ', text)
+    text = WHITESPACE_RE.sub(' ', text).strip()
+    text = text.lower()
+    return text
+
+
+def first_content(entry):
+    return next((
+        c.value
+        for c in (entry.content or ())
+        if c.type == 'text/html'
+    ), None)
+
+
+def is_duplicate(one, two):
+    same_title = False
+    if one.title and two.title:
+        same_title = normalize(one.title or '') == normalize(two.title or '')
+
+    same_text = False
+    if one.summary and two.summary:
+        same_text = normalize(one.summary) == normalize(two.summary)
+    else:
+        one_content = first_content(one)
+        two_content = first_content(two)
+        if one_content and two_content:
+            same_text = normalize(one_content) == normalize(two_content)
+
+    return same_title and same_text
+
+
+def feed_entry_dedupe_plugin(reader, feed, entry):
+    duplicates = [
+        e for e in reader.get_entries(feed=feed)
+        if e.id != entry.id and is_duplicate(entry, e)
+    ]
+    if not duplicates:
+        return
+    if all(d.read for d in duplicates):
+        log.info("%r (%s): found read duplicates, marking this as read",
+                 (feed.url, entry.id), entry.title)
+        reader.mark_as_read((feed.url, entry.id))
+    else:
+        for duplicate in duplicates:
+            reader.mark_as_read(duplicate)
+        log.info("%r (%s): found unread duplicates, marking duplicates as read",
+                 (feed.url, entry.id), entry.title)
+
+
+def feed_entry_dedupe(reader):
+    reader._post_entry_add_plugins.append(feed_entry_dedupe_plugin)
+
+
---
+++ tests/test_plugins_feed_entry_dedupe.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+
+import pytest
+
+from reader.plugins.feed_entry_dedupe import feed_entry_dedupe, normalize, is_duplicate
+from reader import Entry, Content
+
+from fakeparser import Parser
+
+
+def test_normalize():
+    assert normalize('\n\n<B>whatever</B>&nbsp; Blah </p>') == 'whatever blah'
+
+
+def make_entry(title=None, summary=None, content=None):
+    entry = Entry('id', None, title=title, summary=summary)
+    if content:
+        entry = entry._replace(content=[Content(*content)])
+    return entry
+
+IS_DUPLICATE_DATA = [
+    (make_entry(), make_entry(), False),
+
+    (make_entry(title='title'),
+     make_entry(title='title'), False),
+    (make_entry(summary='summary'),
+     make_entry(summary='summary'), False),
+    (make_entry(content=('value', 'text/html')),
+     make_entry(content=('value', 'text/html')), False),
+
+    (make_entry(title='title', summary='summary'),
+     make_entry(title='title', summary='summary'), True),
+    (make_entry(title='title', summary='summary'),
+     make_entry(title='other', summary='summary'), False),
+    (make_entry(title='title', summary='summary'),
+     make_entry(title='title', summary='other'), False),
+
+    (make_entry(title='title', content=('value', 'text/html')),
+     make_entry(title='title', content=('value', 'text/html')), True),
+    (make_entry(title='title', content=('value', 'text/html')),
+     make_entry(title='other', content=('value', 'text/html')), False),
+    (make_entry(title='title', content=('value', 'text/html')),
+     make_entry(title='title', content=('other', 'text/html')), False),
+
+    (make_entry(title='title', content=('value', 'text/plain')),
+     make_entry(title='title', content=('value', 'text/plain')), False),
+    (make_entry(title='title', summary='value'),
+     make_entry(title='title', content=('value', 'text/html')), False),
+]
+
+@pytest.mark.parametrize('one, two, result', IS_DUPLICATE_DATA)
+def test_is_duplicate(one, two, result):
+    assert bool(is_duplicate(one, two)) is bool(result)
+
+
+def test_feed_entry_dedupe(reader, monkeypatch, tmpdir):
+    parser = Parser()
+    reader._parse = parser
+
+    one = parser.feed(1, datetime(2010, 1, 1))
+    old = parser.entry(1, 1, datetime(2010, 1, 1), title='title', summary='old')
+    title_only_one = parser.entry(1, 2, datetime(2010, 1, 1), title='title only')
+    read_one = parser.entry(1, 3, datetime(2010, 1, 1), title='title', summary='read')
+    unread_one = parser.entry(1, 4, datetime(2010, 1, 1), title='title', summary='unread')
+
+    reader.add_feed(one.url)
+    reader.update_feeds()
+    reader.mark_as_read((one.url, read_one.id))
+
+    feed_entry_dedupe(reader)
+
+    one = parser.feed(1, datetime(2010, 1, 2))
+    new = parser.entry(1, 5, datetime(2010, 1, 2), title='title', summary='new')
+    title_only_two = parser.entry(1, 6, datetime(2010, 1, 2), title='title only')
+    read_two = parser.entry(1, 7, datetime(2010, 1, 2), title='title', summary='read')
+    unread_two = parser.entry(1, 8, datetime(2010, 1, 2), title='title', summary='unread')
+
+    reader.update_feeds()
+
+    assert set(reader.get_entries()) == {
+        e._replace(feed=one) for e in {
+            # remain untouched
+            old, new,
+            # also remain untouched
+            title_only_one, title_only_two,
+            # the new one is marked as read because the old one was
+            read_one._replace(read=True), read_two._replace(read=True),
+            # the old one is marked as read in favor of the new one
+            unread_one._replace(read=True), unread_two,
+        }
+    }
+
+
