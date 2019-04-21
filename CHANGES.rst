8595bca182a3a86de738bc6d864892f8f99e98e5
Adrian Damian <damian.adrian24@gmail.com>
2019-04-21 20:12:54 +0300

    Bump version to 0.9.dev0.

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,6 +5,12 @@ reader changelog
 .. module:: reader
 
 
+Version 0.9
+-----------
+
+Unreleased
+
+
 Version 0.8
 -----------
 
--- src/reader/__init__.py
+++ src/reader/__init__.py
@@ -37,7 +37,7 @@ Print the titles of the unread entries::
 
 """
 
-__version__ = '0.8'
+__version__ = '0.9.dev0'
 
 
 from .core.reader import Reader
