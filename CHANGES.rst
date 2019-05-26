dfa9c366af82fd758757f627a03da57917a6c6ee
Adrian Damian <damian.adrian24@gmail.com>
2019-05-26 23:53:36 +0300

    Bump version to 0.12.dev0.

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,6 +5,12 @@ reader changelog
 .. module:: reader
 
 
+Version 0.12
+------------
+
+Unreleased
+
+
 Version 0.11
 ------------
 
--- src/reader/__init__.py
+++ src/reader/__init__.py
@@ -37,7 +37,7 @@ Print the titles of the unread entries::
 
 """
 
-__version__ = '0.11'
+__version__ = '0.12.dev0'
 
 
 from .core.reader import Reader
