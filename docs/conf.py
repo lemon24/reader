d95665d9a68e4ef825b88effba8db10e580fe2af
Adrian Damian <damian.adrian24@gmail.com>
2019-06-22 14:35:37 +0300

    More black.

--- docs/conf.py
+++ docs/conf.py
@@ -115,15 +115,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -133,8 +130,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'reader.tex', 'reader Documentation',
-     'lemon24', 'manual'),
+    (master_doc, 'reader.tex', 'reader Documentation', 'lemon24', 'manual')
 ]
 
 
@@ -142,10 +138,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'reader', 'reader Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, 'reader', 'reader Documentation', [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -154,9 +147,15 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'reader', 'reader Documentation',
-     author, 'reader', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        'reader',
+        'reader Documentation',
+        author,
+        'reader',
+        'One line description of project.',
+        'Miscellaneous',
+    )
 ]
 
 
