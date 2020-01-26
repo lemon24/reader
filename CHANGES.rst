ad3aec399c12ac69a8af332ce5c0e7b32725fad6
lemon24 <damian.adrian24@gmail.com>
2020-01-26 13:11:13 +0200

    Bump version to 0.19.dev0.

--- CHANGES.rst
+++ CHANGES.rst
@@ -6,6 +6,12 @@ reader changelog
   :noindex:
 
 
+Version 0.19
+------------
+
+Unreleased
+
+
 Version 0.18
 ------------
 
--- src/reader/__init__.py
+++ src/reader/__init__.py
@@ -37,7 +37,7 @@ Print the titles of the unread entries::
 
 """
 
-__version__ = '0.18'
+__version__ = '0.19.dev0'
 
 
 from .core.reader import Reader, make_reader
