10c6bf76e6706bef964c93a3b833186d0a0fbb70
lemon24 <damian.adrian24@gmail.com>
2020-04-08 18:13:06 +0300

    Unify the HighlightedString split/apply test dataset.
    For #122.

--- tests/test_types.py
+++ tests/test_types.py
@@ -55,16 +55,8 @@ def test_entry_argument():
         entry_argument(('a', 'b', 'c'))
 
 
-# TODO: re-use the params below
 @pytest.mark.parametrize(
-    'string', [HighlightedString('abcd'), HighlightedString('abcd', [slice(0, 4)]),]
-)
-def test_highlighted_string_str(string):
-    assert str(string) == string.value
-
-
-@pytest.mark.parametrize(
-    'slices',
+    'highlights',
     [
         [slice(0, None)],
         [slice(None, 0)],
@@ -80,23 +72,24 @@ def test_highlighted_string_str(string):
         [slice(0, 2), slice(1, 3)],
     ],
 )
-def test_highlighted_string_slice_validation(slices):
+def test_highlighted_string_slice_validation(highlights):
     with pytest.raises(ValueError):
-        HighlightedString('abcd', slices)
+        HighlightedString('abcd', highlights)
 
 
-@pytest.mark.parametrize(
-    'input, value, highlights',
-    [
-        ('', '', []),
-        (' one ', ' one ', []),
-        ('><one', 'one', ['']),
-        ('><><one', 'one', ['', '']),
-        ('\t >one\n< ', '\t one\n ', ['one\n']),
-        ('>one< two >three< four', 'one two three four', ['one', 'three']),
-        ('one >two< three >four<', 'one two three four', ['two', 'four']),
-    ],
-)
+HS_EXTRACT_DATA = [
+    # input, value, highlights
+    ('', '', []),
+    (' one ', ' one ', []),
+    ('><one', 'one', ['']),
+    ('><><one', 'one', ['', '']),
+    ('\t >one\n< ', '\t one\n ', ['one\n']),
+    ('>one< two >three< four', 'one two three four', ['one', 'three']),
+    ('one >two< three >four<', 'one two three four', ['two', 'four']),
+]
+
+
+@pytest.mark.parametrize('input, value, highlights', HS_EXTRACT_DATA)
 def test_highlighted_string_extract(input, value, highlights):
     string = HighlightedString.extract(input, '>', '<')
     assert string.value == value
@@ -113,66 +106,78 @@ def test_highlighted_string_extract_errors(input):
         HighlightedString.extract(input, '>', '<')
 
 
-@pytest.mark.parametrize(
-    'string, expected',
-    [
-        (HighlightedString(), ['']),
-        (HighlightedString('abcd'), ['abcd']),
-        (HighlightedString('abcd', [slice(0, 4)]), ['', 'abcd', '']),
-        (HighlightedString('abcd', [slice(0, 0)]), ['', '', 'abcd']),
-        (HighlightedString('abcd', [slice(4, 4)]), ['abcd', '', '']),
-        (HighlightedString('abcd', [slice(2, 2)]), ['ab', '', 'cd']),
-        (HighlightedString('abcd', [slice(1, 3)]), ['a', 'bc', 'd']),
-        (
-            HighlightedString('abcd', [slice(0, 0), slice(0, 0)]),
-            ['', '', '', '', 'abcd'],
-        ),
-        (
-            HighlightedString('abcd', [slice(1, 2), slice(2, 3)]),
-            ['a', 'b', '', 'c', 'd'],
-        ),
-        (
-            HighlightedString('abcd', [slice(2, 3), slice(1, 2)]),
-            ['a', 'b', '', 'c', 'd'],
-        ),
-        (
-            HighlightedString('abcd', [slice(1, 2), slice(3, 4)]),
-            ['a', 'b', 'c', 'd', ''],
-        ),
-        (
-            HighlightedString('abcd', [slice(0, 1), slice(2, 3)]),
-            ['', 'a', 'b', 'c', 'd'],
-        ),
-        (
-            HighlightedString(
-                'one two three four', [slice(0, 3, None), slice(8, 13, None)]
-            ),
-            ['', 'one', ' two ', 'three', ' four'],
-        ),
-    ],
-)
+HS_SPLIT_APPLY_DATA = [
+    # string, split, apply, apply_upper
+    (HighlightedString(), [''], '', ''),
+    (HighlightedString('abcd'), ['abcd'], 'abcd', 'ABCD'),
+    (HighlightedString('abcd', [slice(0, 4)]), ['', 'abcd', ''], 'xabcdy', 'xABCDy'),
+    (HighlightedString('abcd', [slice(0, 0)]), ['', '', 'abcd'], 'xyabcd', 'xyABCD'),
+    (HighlightedString('abcd', [slice(4, 4)]), ['abcd', '', ''], 'abcdxy', 'ABCDxy'),
+    (HighlightedString('abcd', [slice(2, 2)]), ['ab', '', 'cd'], 'abxycd', 'ABxyCD'),
+    (HighlightedString('abcd', [slice(1, 3)]), ['a', 'bc', 'd'], 'axbcyd', 'AxBCyD'),
+    (
+        HighlightedString('abcd', [slice(0, 0), slice(0, 0)]),
+        ['', '', '', '', 'abcd'],
+        'xyxyabcd',
+        'xyxyABCD',
+    ),
+    (
+        HighlightedString('abcd', [slice(1, 2), slice(2, 3)]),
+        ['a', 'b', '', 'c', 'd'],
+        'axbyxcyd',
+        'AxByxCyD',
+    ),
+    (
+        HighlightedString('abcd', [slice(2, 3), slice(1, 2)]),
+        ['a', 'b', '', 'c', 'd'],
+        'axbyxcyd',
+        'AxByxCyD',
+    ),
+    (
+        HighlightedString('abcd', [slice(1, 2), slice(3, 4)]),
+        ['a', 'b', 'c', 'd', ''],
+        'axbycxdy',
+        'AxByCxDy',
+    ),
+    (
+        HighlightedString('abcd', [slice(0, 1), slice(2, 3)]),
+        ['', 'a', 'b', 'c', 'd'],
+        'xaybxcyd',
+        'xAyBxCyD',
+    ),
+    (
+        HighlightedString('one two three four', [slice(0, 3), slice(8, 13)]),
+        ['', 'one', ' two ', 'three', ' four'],
+        'xoney two xthreey four',
+        'xONEy TWO xTHREEy FOUR',
+    ),
+]
+
+
+@pytest.mark.parametrize('string, expected', [t[:2] for t in HS_SPLIT_APPLY_DATA])
 def test_highlighted_string_split(string, expected):
     assert list(string.split()) == expected
 
 
 @pytest.mark.parametrize(
-    'string, expected, expected_upper',
-    [
-        (HighlightedString(), '', ''),
-        (HighlightedString('abcd'), 'abcd', 'ABCD'),
-        (HighlightedString('abcd', [slice(0, 4)]), 'xabcdy', 'xABCDy'),
-        (HighlightedString('abcd', [slice(0, 0)]), 'xyabcd', 'xyABCD'),
-        (HighlightedString('abcd', [slice(4, 4)]), 'abcdxy', 'ABCDxy'),
-        (HighlightedString('abcd', [slice(2, 2)]), 'abxycd', 'ABxyCD'),
-        (HighlightedString('abcd', [slice(1, 3)]), 'axbcyd', 'AxBCyD'),
-        (HighlightedString('abcd', [slice(0, 0), slice(0, 0)]), 'xyxyabcd', 'xyxyABCD'),
-        (HighlightedString('abcd', [slice(1, 2), slice(2, 3)]), 'axbyxcyd', 'AxByxCyD'),
-        (HighlightedString('abcd', [slice(2, 3), slice(1, 2)]), 'axbyxcyd', 'AxByxCyD'),
-        (HighlightedString('abcd', [slice(1, 2), slice(3, 4)]), 'axbycxdy', 'AxByCxDy'),
-        (HighlightedString('abcd', [slice(0, 1), slice(2, 3)]), 'xaybxcyd', 'xAyBxCyD'),
-    ],
+    'string, expected, expected_upper', [t[:1] + t[2:] for t in HS_SPLIT_APPLY_DATA]
 )
 def test_highlighted_string_apply(string, expected, expected_upper):
     assert string.apply('', '') == string.value
     assert string.apply('x', 'y') == expected
     assert string.apply('x', 'y', str.upper) == expected_upper
+
+
+@pytest.mark.parametrize('string', [t[0] for t in HS_SPLIT_APPLY_DATA])
+def test_highlighted_string_str(string):
+    assert str(string) == string.value
+
+
+@pytest.mark.parametrize(
+    'input, before, after',
+    [(t[0], '>', '<') for t in HS_EXTRACT_DATA]
+    + [(t[2], 'x', 'y') for t in HS_SPLIT_APPLY_DATA]
+    + [(t[3], 'x', 'y') for t in HS_SPLIT_APPLY_DATA],
+)
+def test_highlighted_string_roundtrip(input, before, after):
+    assert HighlightedString.extract(input, before, after).apply(before, after) == input
