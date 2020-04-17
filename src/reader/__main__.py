618f158ebc0034eefb724a55a84937d21c93c1a7
lemon24 <damian.adrian24@gmail.com>
2020-04-17 21:48:00 +0300

    Make 'python -m reader' show the correct program name.

--- src/reader/__main__.py
+++ src/reader/__main__.py
@@ -1,20 +1,18 @@
-if __name__ == '__main__':
-    try:
-        from reader._cli import cli
+import sys
 
-        cli()
-    except ImportError:
-        import sys
-
-        print(
-            """\
+CANNOT_IMPORT = """\
 Error: cannot import reader._cli
 
 This might be due to missing dependencies. The command-line interface is
 optional, use the 'cli' extra to install its dependencies:
 
     pip install reader[cli]
-""",
-            file=sys.stderr,
-        )
-        raise
+"""
+
+try:
+    from reader._cli import cli
+
+    cli(prog_name='python -m reader')
+except ImportError:
+    print(CANNOT_IMPORT, file=sys.stderr)
+    raise
