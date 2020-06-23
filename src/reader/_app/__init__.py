lemon24/reader

commit 0bd9e9c442e3a8078df78b73f49e97cf0f3aecc9

Handle invalid feeds in /preview.
For #172.

src/reader/_app/__init__.py
@@ -209,8 +209,10 @@ def preview():
     reader = make_reader(':memory:')
     reader.add_feed(url)
 
-    reader.update_feed(url)
-    # TODO: handle ParseError nicely
+    # update_feeds(), not update_feed(), because it handles errors for us
+    # (feed.last_exception will get checked in the template);
+    # https://github.com/lemon24/reader/issues/172
+    reader.update_feeds(url)
 
     feed = reader.get_feed(url)
     entries = list(reader.get_entries())

commit caa45748772cfa33d90c04e37de56b9b5e22f9b9

Fix preview_feed_list broken in 0bd9e9c; for #172.

src/reader/_app/__init__.py
@@ -4,6 +4,7 @@ import json
 import time
 from dataclasses import dataclass
 
+import flask.signals
 import humanize
 import markupsafe
 from flask import abort
@@ -34,6 +35,12 @@ from reader._plugins import LoaderError
 blueprint = Blueprint('reader', __name__)
 
 blueprint.app_template_filter('humanize_naturaltime')(humanize.naturaltime)
+
+# if any plugins need signals, they need to install blinker
+signals = flask.signals.Namespace()
+
+# NOTE: these signals are part of the app extension API
+got_preview_parse_error = signals.signal('preview-parse-error')
 
 
 def get_reader():
@@ -209,10 +216,16 @@ def preview():
     reader = make_reader(':memory:')
     reader.add_feed(url)
 
-    # update_feeds(), not update_feed(), because it handles errors for us
-    # (feed.last_exception will get checked in the template);
+    try:
+        reader.update_feed(url)
+    except ParseError as e:
+        # give plugins a chance to intercept this
+        got_preview_parse_error.send(e)
+
     # https://github.com/lemon24/reader/issues/172
-    reader.update_feeds(url)
+    # no plugin intercepted the response, so we show the feed;
+    # feed.last_exception will be checked in the template,
+    # and if there was a ParseError, it will be shown
 
     feed = reader.get_feed(url)
     entries = list(reader.get_entries())
@@ -414,6 +427,7 @@ def create_app(db_path, plugins=(), app_plugins=()):
 
     app.register_blueprint(blueprint)
 
+    # NOTE: this is part of the app extension API
     app.reader_additional_enclosure_links = []
 
     app.reader_load_plugins = FlaskPluginLoader(plugins).load_plugins

src/reader/_plugins/preview_feed_list.py
@@ -30,8 +30,8 @@ from flask import render_template
 from flask import request
 from flask import url_for
 
-import reader
 from reader._app import get_reader
+from reader._app import got_preview_parse_error
 
 
 blueprint = Blueprint('preview_feed_list', __name__, template_folder='templates')
@@ -42,6 +42,10 @@ def feed_list():
     url = request.args['url']
 
     session = get_reader()._parser.make_session()
+
+    # TODO: url may not actually be an http URL; now we get "error: Invalid URL 'file.xml': No schema supplied. ..."
+    # if https://github.com/lemon24/reader/issues/155#issuecomment-647048623 gets implemented,
+    # we should delegate to the parser "give me the content of this URL"
 
     try:
         response = session.get(url)
@@ -69,8 +73,19 @@ def feed_list():
     return render_template('preview_feed_list.html', url=url, alternates=alternates)
 
 
-@blueprint.app_errorhandler(reader.ParseError)
+class GotPreviewParseError(Exception):
+    """Signaling exception used to intercept /preview ParseError"""
+
+
+@got_preview_parse_error.connect
+def raise_got_preview_parse_error(error):
+    raise GotPreviewParseError() from error
+
+
+@blueprint.app_errorhandler(GotPreviewParseError)
 def handle_parse_error_i_guess(error):
+    parse_error = error.__cause__
+
     if request.url_rule.endpoint != 'reader.preview':
         raise error
 
@@ -79,7 +94,7 @@ def handle_parse_error_i_guess(error):
     # TODO: maybe we should still expose the response on the exception, we could at least reuse the status code
     # TODO: ParseError should be more specific, it should be clear if retrieving or parsing failed
 
-    return redirect(url_for('preview_feed_list.feed_list', url=error.url))
+    return redirect(url_for('preview_feed_list.feed_list', url=parse_error.url))
 
 
 def init(app):