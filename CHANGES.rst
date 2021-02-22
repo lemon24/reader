189d71556165ce56ac54058232d5fdfc230ee3f4
lemon24 <damian.adrian24@gmail.com>
2021-02-22 16:30:04 +0200

    Bump version to 1.15.dev0.

--- CHANGES.rst
+++ CHANGES.rst
@@ -6,6 +6,12 @@ Changelog
   :noindex:
 
 
+Version 1.15
+------------
+
+Unreleased
+
+
 Version 1.14
 ------------
 
--- src/reader/__init__.py
+++ src/reader/__init__.py
@@ -37,7 +37,7 @@ Print the titles of the unread entries::
 
 """
 
-__version__ = '1.14'
+__version__ = '1.15.dev0'
 
 from .core import Reader, make_reader
 
