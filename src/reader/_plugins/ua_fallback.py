eb65616779a6d75ce6a760810844a0c54e8526fc
lemon24 <damian.adrian24@gmail.com>
2021-03-25 11:50:02 +0200

    Keep _plugins.ua_fallback temporarily.
    For #229.

---
+++ src/reader/_plugins/ua_fallback.py
@@ -0,0 +1,7 @@
+# For backwards compatibility.
+#
+# Remove before releasing 1.16 if there's no reply to
+# https://github.com/lemon24/reader/issues/226
+#
+# If there is, remove in reader 2.0.
+from reader.plugins.ua_fallback import init_reader as init  # noqa
