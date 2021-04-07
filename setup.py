889b6cba07d0a55e6fb27c0807639d2b39cae5d1
lemon24 <damian.adrian24@gmail.com>
2021-04-07 15:45:39 +0300

    Move most package metadata to setup.cfg, and add pyproject.toml.
    For #232.

--- setup.py
+++ setup.py
@@ -1,29 +1,9 @@
-import ast
-import re
-
-from setuptools import find_packages
 from setuptools import setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('src/reader/__init__.py', 'rb') as f:
-    version = str(
-        ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1))
-    )
-
-with open('README.rst') as f:
-    long_description = f.read()
-
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+# TODO: move to setup.cfg once this issue is resolved: https://github.com/dependabot/dependabot-core/issues/2133
 setup(
     name='reader',
-    version=version,
-    author='lemon24',
-    url='https://github.com/lemon24/reader',
-    license='BSD',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    python_requires='>=3.6',
     install_requires=[
         'dataclasses; python_version<"3.7"',
         'typing-extensions',
@@ -86,36 +66,4 @@ setup(
             'sphinx-hoverxref',
         ],
     },
-    description="A minimal feed reader library.",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    project_urls={
-        "Documentation": "https://reader.readthedocs.io/",
-        "Code": "https://github.com/lemon24/reader",
-        "Issue tracker": "https://github.com/lemon24/reader/issues",
-    },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
-        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Utilities',
-    ],
 )
