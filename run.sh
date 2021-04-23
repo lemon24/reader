dca9f0a44e04b77d95a931b7eb2c3ddc8b860045
lemon24 <damian.adrian24@gmail.com>
2021-04-23 19:23:39 +0300

    Add run.sh.

---
+++ run.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+#
+# usage: ./run.sh command [argument ...]
+#
+# Commands for use during development / CI.
+#
+
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+
+# Change the current directory to the project root.
+script_root=${0%/*}
+if [[ $0 != $script_root && $script_root != "" ]]; then
+    cd "$script_root"
+fi
+unset script_root
+
+# Store the absolute path to this script (useful for recursion).
+readonly SCRIPT="$( pwd )/$( basename "$0" )"
+
+
+function install-dev {
+    pip install -e '.[search,cli,app,enclosure-tags,preview-feed-list,dev,docs]'
+}
+
+
+function coverage-all {
+    local options=$( getopt x "$@" )
+    eval set -- "$options"
+
+    local context=true
+
+    while true; do
+        case "$1" in
+        -x) context= ;;
+        --) break ;;
+        esac
+        shift
+    done
+
+    if [[ $context == true ]]; then
+        coverage-run --cov-context=test
+        coverage-report --show-contexts
+    else
+        coverage-run
+        coverage-report
+    fi
+}
+
+function coverage-run {
+    clean-pyc
+    pytest --cov --runslow "$@"
+}
+
+function coverage-report {
+    coverage html "$@"
+    coverage report \
+        --include '*/reader/*' \
+        --omit "$(
+            echo "
+                */reader/_vendor/*
+                */reader/__main__.py
+                */reader/_cli*
+                */reader/_config*
+                */reader/_app/*
+                */reader/_plugins/*
+                tests/*
+            " | xargs echo | sed 's/ /,/g'
+        )" \
+        --skip-covered \
+        --fail-under 100
+}
+
+
+function typing {
+    local implementation=$( python -c 'import sys; print(sys.implementation.name)' )
+
+    if [[ $implementation == pypy ]]; then
+        # mypy does not work on pypy as of January 2020
+        # https://github.com/python/typed_ast/issues/97#issuecomment-484335190
+        echo "mypy does not work on pypy, doing nothing"
+        return
+    fi
+
+    mypy --strict src "$@"
+}
+
+
+function docs {
+    make -C docs html "$@"
+}
+
+
+function ls-project-files {
+    git ls-files "$@"
+    git ls-files --exclude-standard --others "$@"
+}
+
+function entr-project-files {
+    set +o errexit
+    while true; do
+        ls-project-files | entr "$@"
+        if [[ $? -eq 0 ]]; then
+            break
+        fi
+    done
+}
+
+
+function test-dev {
+    clean-pyc
+    entr-project-files -cdr pytest "$@"
+}
+
+function typing-dev {
+    entr-project-files -cdr "$SCRIPT" typing "$@"
+}
+
+function docs-dev {
+    make -C docs clean
+    entr-project-files -cdr "$SCRIPT" docs "$@"
+}
+
+function serve-dev {
+    export FLASK_DEBUG=1
+    export FLASK_TRAP_BAD_REQUEST_ERRORS=1
+    export FLASK_APP=src/reader/_app/wsgi.py
+    export READER_DB=db.sqlite
+    flask run -h 0.0.0.0 -p 8000
+}
+
+
+function clean-pyc {
+    find \
+        docs examples scripts src tests \
+        -name '*.pyc' -or -name '*.pyo' \
+        -exec rm -rf {} +
+}
+
+
+# Pass control to commands.
+"$@"
