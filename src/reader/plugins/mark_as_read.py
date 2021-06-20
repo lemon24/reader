9a48ccc933fafe28ca8f6fff1f5f694ef439d362
lemon24 <damian.adrian24@gmail.com>
2021-06-20 10:38:10 +0300

    Fix wrong mark_as_read plugin name.

--- src/reader/plugins/mark_as_read.py
+++ src/reader/plugins/mark_as_read.py
@@ -1,11 +1,11 @@
 """
-reader.mark_entry_as_read
-~~~~~~~~~~~~~~~~~~~~~~~~~
+reader.mark_as_read
+~~~~~~~~~~~~~~~~~~~
 
 Mark added entries of specific feeds as read if their title matches a regex.
 
 To configure, set the ``make_reader_reserved_name('mark_as_read')``
-(by default, ``.reader.mark_entry_as_read``)
+(by default, ``.reader.mark_as_read``)
 feed metadata to something like::
 
     {
