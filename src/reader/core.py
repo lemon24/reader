c02a4b18fd336d2c1b94c25a941d5d87b7db5bdd
lemon24 <damian.adrian24@gmail.com>
2021-10-30 20:52:47 +0300

    Revert "Prorotype for #261."
    This reverts commit b044addf6161f307221f0ac05acde1f4eb14c3f2.

--- src/reader/core.py
+++ src/reader/core.py
@@ -789,7 +789,6 @@ class Reader:
         *,
         new: Optional[bool] = None,
         workers: int = 1,
-        url=None,
     ) -> Iterable[UpdateResult]:
         """Update all the feeds that have updates enabled.
 
@@ -835,7 +834,7 @@ class Reader:
         make_map = nullcontext(builtins.map) if workers == 1 else make_pool_map(workers)
 
         with make_map as map:
-            results = self._update_feeds(new=new, map=map, url=url)
+            results = self._update_feeds(new=new, map=map)
 
             for url, value in results:
                 if isinstance(value, FeedNotFoundError):
@@ -937,30 +936,9 @@ class Reader:
             self._updater.process_old_feed, feeds_for_update
         )
 
-        def _retrieve(feed):
-            try:
-                return feed, self._parser.retrieve(
-                    feed.url, feed.http_etag, feed.http_last_modified
-                )
-            except ParseError as e:
-                print(e)
-                return feed, e
+        pairs = map(self._parse_feed_for_update, feeds_for_update)
 
-        retrieve_results = map(_retrieve, feeds_for_update)
-
-        # for feed_for_update, parse_result in pairs:
-
-        for feed_for_update, retrieve_cm in retrieve_results:
-            if hasattr(retrieve_cm, '__enter__'):
-
-                try:
-                    with retrieve_cm as result:
-                        parse_result = self._parser.parse(feed_for_update.url, result)
-                except ParseError as e:
-                    parse_result = e
-            else:
-                parse_result = retrieve_cm
-
+        for feed_for_update, parse_result in pairs:
             rv: Union[UpdatedFeed, None, Exception]
 
             try:
