43d9f00742ba614cce4c6a9b1c024f53f6b32e2e
lemon24 <damian.adrian24@gmail.com>
2023-08-20 11:48:22 +0300

    Bump version to 3.9.dev0.

--- CHANGES.rst
+++ CHANGES.rst
@@ -6,6 +6,12 @@ Changelog
   :noindex:
 
 
+Version 3.9
+-----------
+
+Unreleased
+
+
 Version 3.8
 -----------
 
--- src/reader/__init__.py
+++ src/reader/__init__.py
@@ -37,7 +37,7 @@ Print the titles of the unread entries::
 
 """
 
-__version__ = '3.8'
+__version__ = '3.9.dev0'
 
 from .core import (
     Reader as Reader,
