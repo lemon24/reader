04b2870830d410cf33dd9685f0c145362176df46
lemon24 <damian.adrian24@gmail.com>
2023-12-06 00:53:08 +0200

    Don't use custom entry ids in test_reader_filter.py. #328

--- tests/test_reader_filter.py
+++ tests/test_reader_filter.py
@@ -44,23 +44,19 @@ TAGS_AND_EXPECTED_IDS = [
 ]
 
 
-def resource_ids_to_int(resource_ids):
-    return {tuple(map(eval, rid)) for rid in resource_ids}
-
-
 @pytest.fixture
 def reader_for_tags(reader):
     reader._parser = parser = Parser()
 
     one = parser.feed(1, datetime(2010, 1, 1))  # tag, first
-    one_one = parser.entry(1, '1', datetime(2010, 1, 1))
-    one_two = parser.entry(1, '2', datetime(2010, 2, 1))
+    one_one = parser.entry(1, 1, datetime(2010, 1, 1))
+    one_two = parser.entry(1, 2, datetime(2010, 2, 1))
 
     two = parser.feed(2, datetime(2010, 1, 1))  # tag, second
-    two_one = parser.entry(2, '1', datetime(2010, 1, 1))
+    two_one = parser.entry(2, 1, datetime(2010, 1, 1))
 
     three = parser.feed(3, datetime(2010, 1, 1))  # <no tags>
-    three_one = parser.entry(3, '1', datetime(2010, 1, 1))
+    three_one = parser.entry(3, 1, datetime(2010, 1, 1))
 
     for feed in one, two, three:
         reader.add_feed(feed)
@@ -79,11 +75,11 @@ def test_entries_by_feed_tags(reader, get_entries, tags, expected):
     reader.set_tag('2', 'tag')
     reader.set_tag('2', 'second')
 
-    resource_ids = {e.resource_id for e in get_entries(reader, feed_tags=tags)}
-    assert resource_ids_to_int(resource_ids) == expected, tags
+    actual = {eval(e.id) for e in get_entries(reader, feed_tags=tags)}
+    assert actual == expected
 
     if tags is None:
-        assert resource_ids == {e.resource_id for e in get_entries(reader)}
+        assert actual == {eval(e.id) for e in get_entries(reader)}
 
 
 # TODO: maybe test all the get_feeds sort orders (maybe fixture?)
@@ -100,11 +96,11 @@ def test_feeds_by_feed_tags(reader, get_feeds, tags, expected):
     reader.set_tag('2', 'tag')
     reader.set_tag('2', 'second')
 
-    resource_ids = {f.resource_id for f in get_feeds(reader, tags=tags)}
-    assert resource_ids_to_int(resource_ids) == {t[:1] for t in expected}, tags
+    actual = {eval(f.url) for f in get_feeds(reader, tags=tags)}
+    assert actual == {t[0] for t in expected}
 
     if tags is None:
-        assert resource_ids == {f.resource_id for f in get_feeds(reader)}
+        assert actual == {eval(f.url) for f in get_feeds(reader)}
 
 
 @pytest.mark.parametrize('tags, expected', TAGS_AND_EXPECTED_IDS)
@@ -112,18 +108,18 @@ def test_feeds_by_feed_tags(reader, get_feeds, tags, expected):
 def test_entries_by_entry_tags(reader, get_entries, tags, expected):
     get_entries.after_update(reader)
 
-    reader.set_tag(('1', '1'), 'tag')
-    reader.set_tag(('1', '2'), 'tag')
-    reader.set_tag(('1', '1'), 'first')
-    reader.set_tag(('1', '2'), 'first')
-    reader.set_tag(('2', '1'), 'tag')
-    reader.set_tag(('2', '1'), 'second')
+    reader.set_tag(('1', '1, 1'), 'tag')
+    reader.set_tag(('1', '1, 2'), 'tag')
+    reader.set_tag(('1', '1, 1'), 'first')
+    reader.set_tag(('1', '1, 2'), 'first')
+    reader.set_tag(('2', '2, 1'), 'tag')
+    reader.set_tag(('2', '2, 1'), 'second')
 
-    resource_ids = {e.resource_id for e in get_entries(reader, tags=tags)}
-    assert resource_ids_to_int(resource_ids) == expected, tags
+    actual = {eval(e.id) for e in get_entries(reader, tags=tags)}
+    assert actual == expected
 
     if tags is None:
-        assert resource_ids == {e.resource_id for e in get_entries(reader)}
+        assert actual == {eval(e.id) for e in get_entries(reader)}
 
 
 # END tag filtering tests
