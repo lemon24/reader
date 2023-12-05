5def87c22b407f0f67e65a0351c696157ef8d23a
lemon24 <damian.adrian24@gmail.com>
2023-12-06 00:37:36 +0200

    Replace filtering test_entry_tags_basic with feed-tags-style test. #328

--- tests/test_reader_filter.py
+++ tests/test_reader_filter.py
@@ -5,6 +5,8 @@ from utils import rename_argument
 from utils import utc_datetime as datetime
 
 
+# BEGIN tag filtering tests
+
 ALL_IDS = {(1, 1), (1, 2), (2, 1), (3, 1)}
 
 TAGS_AND_EXPECTED_IDS = [
@@ -67,20 +69,15 @@ def reader_for_tags(reader):
     return reader
 
 
-@pytest.fixture
+@pytest.mark.parametrize('tags, expected', TAGS_AND_EXPECTED_IDS)
 @rename_argument('reader', 'reader_for_tags')
-def reader_feed_tags(reader):
+def test_entries_by_feed_tags(reader, get_entries, tags, expected):
+    get_entries.after_update(reader)
+
     reader.set_tag('1', 'tag')
     reader.set_tag('1', 'first')
     reader.set_tag('2', 'tag')
     reader.set_tag('2', 'second')
-    return reader
-
-
-@pytest.mark.parametrize('tags, expected', TAGS_AND_EXPECTED_IDS)
-@rename_argument('reader', 'reader_feed_tags')
-def test_entries_by_feed_tags(reader, get_entries, tags, expected):
-    get_entries.after_update(reader)
 
     resource_ids = [e.resource_id for e in get_entries(reader, feed_tags=tags)]
     assert resource_ids_to_int(resource_ids) == expected, tags
@@ -94,9 +91,14 @@ def test_entries_by_feed_tags(reader, get_entries, tags, expected):
 
 @pytest.mark.parametrize('get_feeds', [get_feeds])
 @pytest.mark.parametrize('tags, expected', TAGS_AND_EXPECTED_IDS)
-@rename_argument('reader', 'reader_feed_tags')
-def test_feeds_by_tags(reader, get_feeds, tags, expected):
+@rename_argument('reader', 'reader_for_tags')
+def test_feeds_by_feed_tags(reader, get_feeds, tags, expected):
     get_feeds.after_update(reader)
+
+    reader.set_tag('1', 'tag')
+    reader.set_tag('1', 'first')
+    reader.set_tag('2', 'tag')
+    reader.set_tag('2', 'second')
 
     resource_ids = [f.resource_id for f in get_feeds(reader, tags=tags)]
     assert resource_ids_to_int(resource_ids) == {t[:1] for t in expected}, tags
@@ -105,42 +107,23 @@ def test_feeds_by_tags(reader, get_feeds, tags, expected):
         assert resource_ids == [f.resource_id for f in get_feeds(reader)]
 
 
-def test_entry_tags_basic(reader):
-    # roughly modeled after test_filtering_tags
+@pytest.mark.parametrize('tags, expected', TAGS_AND_EXPECTED_IDS)
+@rename_argument('reader', 'reader_for_tags')
+def test_entries_by_entry_tags(reader, get_entries, tags, expected):
+    get_entries.after_update(reader)
 
-    reader._parser = parser = Parser()
+    reader.set_tag(('1', '1'), 'tag')
+    reader.set_tag(('1', '2'), 'tag')
+    reader.set_tag(('1', '1'), 'first')
+    reader.set_tag(('1', '2'), 'first')
+    reader.set_tag(('2', '1'), 'tag')
+    reader.set_tag(('2', '1'), 'second')
 
-    one = parser.feed(1, datetime(2010, 1, 1))
-    one_one = parser.entry(1, 1, datetime(2010, 1, 1))
-    one_two = parser.entry(1, 2, datetime(2010, 2, 1))
-    two = parser.feed(2, datetime(2010, 1, 1))
-    two_one = parser.entry(2, 1, datetime(2010, 1, 1))
+    resource_ids = [e.resource_id for e in get_entries(reader, tags=tags)]
+    assert resource_ids_to_int(resource_ids) == expected, tags
 
-    for feed in one, two:
-        reader.add_feed(feed)
+    if tags is None:
+        assert resource_ids == [e.resource_id for e in get_entries(reader)]
 
-    reader.update_feeds()
 
-    reader.set_tag(one_one, 'tag')
-    reader.set_tag(one_one, 'first')
-    reader.set_tag(two_one, 'tag')
-    reader.set_tag(two_one, 'second')
-
-    def get(tags):
-        return {e.id for e in reader.get_entries(tags=tags)}
-
-    assert get(None) == {'1, 1', '1, 2', '2, 1'}
-    assert get(['tag']) == {'1, 1', '2, 1'}
-    assert get(['first']) == {'1, 1'}
-    assert get(['second']) == {'2, 1'}
-    assert get(True) == {'1, 1', '2, 1'}
-    assert get(False) == {'1, 2'}
-    assert get(['unknown']) == set()
-
-    def count(tags):
-        return reader.get_entry_counts(tags=tags).total
-
-    assert count(None) == 3
-    assert count(['tag']) == 2
-    assert count(['first']) == 1
-    assert count(['unknown']) == 0
+# END tag filtering tests
