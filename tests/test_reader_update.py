9b266bd368e7cd8bae6dd15e9ce4a9a180aa10c5
lemon24 <damian.adrian24@gmail.com>
2026-01-02 20:37:17 +0200

    Move / clean up some update tests to test_reader_update.py.

--- tests/test_reader_update.py
+++ tests/test_reader_update.py
@@ -6,12 +6,283 @@ TODO: move all update tests from test_reader.py here
 """
 
 import threading
+from unittest.mock import ANY
 
 import pytest
 
-from fakeparser import Parser
 from utils import Blocking
 from utils import utc_datetime as datetime
+
+
+# BEGIN: should (not) update
+
+# fmt: off
+
+def prepare_feed(reader):
+    feed = reader._parser.feed(1, datetime(2010, 1, 1), title='old')
+    reader._now = lambda: datetime(2010, 1, 1)
+    reader.add_feed(feed.url)
+    reader._now = lambda: datetime(2010, 1, 2)
+    return feed
+
+def entry_is_added(reader):
+    return reader._parser.entry(1, 1, datetime(2010, 1, 1), title='one')
+
+def prepare_feed_and_entry(reader):
+    return prepare_feed(reader), entry_is_added(reader)
+
+def nothing_changes(reader):
+    pass
+
+def feed_changes(reader):
+    reader._parser.feed(1, datetime(2010, 1, 1), title='new')
+
+def feed_updated_is_older(reader):
+    reader._parser.feed(1, datetime(2009, 12, 31), title='old')
+
+def feed_updated_is_newer(reader):
+    reader._parser.feed(1, datetime(2010, 1, 2), title='old')
+
+def entry_changes(reader):
+    reader._parser.entry(1, 1, datetime(2010, 1, 1), title='ONE')
+
+def entry_updated_is_older(reader):
+    reader._parser.entry(1, 1, datetime(2009, 12, 31), title='one')
+
+def entry_updated_is_newer(reader):
+    reader._parser.entry(1, 1, datetime(2010, 1, 2), title='one')
+
+def second_entry_is_added(reader):
+    return reader._parser.entry(1, 2, datetime(2010, 1, 1), title='two')
+
+def feed_updated_is_older_and_entry_changes(reader):
+    reader._parser.feed(1, datetime(2009, 12, 31), title='old')
+    reader._parser.entry(1, 1, datetime(2010, 1, 1), title='ONE')
+
+def feed_updated_is_newer_and_entry_changes(reader):
+    reader._parser.feed(1, datetime(2010, 1, 2), title='old')
+    reader._parser.entry(1, 1, datetime(2010, 1, 1), title='ONE')
+
+# fmt: on
+
+
+@pytest.mark.noscheduled
+def test_basic(reader, parser, update_feed, subtests):
+
+    with subtests.test("empty feed"):
+        feed = prepare_feed(reader)
+        expected_feed = feed.as_feed(
+            added=datetime(2010, 1, 1),
+            last_updated=datetime(2010, 1, 2),
+            last_retrieved=datetime(2010, 1, 2),
+            update_after=datetime(2010, 1, 2, 1),
+        )
+
+        update_feed(reader, feed)
+
+        assert reader.get_feed(feed) == expected_feed
+        assert set(reader.get_entries()) == set()
+
+    with subtests.test("add first entry"):
+        one = entry_is_added(reader)
+
+        update_feed(reader, feed)
+
+        assert reader.get_feed(feed) == expected_feed
+        assert set(reader.get_entries()) == {
+            one.as_entry(
+                feed=expected_feed,
+                added=datetime(2010, 1, 2),
+                last_updated=datetime(2010, 1, 2),
+            ),
+        }
+
+    with subtests.test("add second entry"):
+        two = second_entry_is_added(reader)
+        reader._now = lambda: datetime(2010, 1, 3)
+        expected_feed = feed.as_feed(
+            added=datetime(2010, 1, 1),
+            last_updated=datetime(2010, 1, 3),
+            last_retrieved=datetime(2010, 1, 3),
+            update_after=datetime(2010, 1, 3, 1),
+        )
+
+        update_feed(reader, feed)
+
+        assert reader.get_feed(feed) == expected_feed
+        assert set(reader.get_entries()) == {
+            one.as_entry(
+                feed=expected_feed,
+                added=datetime(2010, 1, 2),
+                last_updated=datetime(2010, 1, 2),
+            ),
+            two.as_entry(
+                feed=expected_feed,
+                added=datetime(2010, 1, 3),
+                last_updated=datetime(2010, 1, 3),
+            ),
+        }
+
+
+@pytest.mark.parametrize(
+    'when',
+    [
+        feed_changes,
+        entry_changes,
+        second_entry_is_added,
+        feed_updated_is_older_and_entry_changes,
+        feed_updated_is_newer_and_entry_changes,
+    ],
+)
+def test_feed_is_updated(reader, parser, when):
+    old_feed, _ = prepare_feed_and_entry(reader)
+    reader.update_feeds()
+
+    reader._now = lambda: datetime(2010, 1, 3)
+    when(reader)
+    reader.update_feeds()
+
+    new_feed = parser.feeds[1]
+    assert reader.get_feed(old_feed) == new_feed.as_feed(
+        added=datetime(2010, 1, 1),
+        last_updated=datetime(2010, 1, 3),
+        last_retrieved=datetime(2010, 1, 3),
+        update_after=datetime(2010, 1, 3, 1),
+    )
+
+
+@pytest.mark.parametrize(
+    'when', [nothing_changes, feed_updated_is_older, feed_updated_is_newer]
+)
+def test_feed_is_not_updated(reader, parser, when):
+    old_feed, _ = prepare_feed_and_entry(reader)
+    reader.update_feeds()
+
+    reader._now = lambda: datetime(2010, 1, 3)
+    when(reader)
+    reader.update_feeds()
+
+    assert reader.get_feed(old_feed) == old_feed.as_feed(
+        added=datetime(2010, 1, 1),
+        last_updated=datetime(2010, 1, 2),
+        last_retrieved=datetime(2010, 1, 3),
+        update_after=datetime(2010, 1, 3, 1),
+    )
+
+
+@pytest.mark.parametrize(
+    'when', [entry_changes, entry_updated_is_older, entry_updated_is_newer]
+)
+def test_entry_is_updated(reader, parser, when):
+    feed, _ = prepare_feed_and_entry(reader)
+    reader.update_feeds()
+
+    reader._now = lambda: datetime(2010, 1, 3)
+    when(reader)
+    reader.update_feeds()
+
+    new_one = parser.entries[1][1]
+    expected_feed = feed.as_feed(
+        added=datetime(2010, 1, 1),
+        last_updated=datetime(2010, 1, 3),
+        last_retrieved=datetime(2010, 1, 3),
+        update_after=datetime(2010, 1, 3, 1),
+    )
+    assert reader.get_entry(new_one) == new_one.as_entry(
+        feed=expected_feed,
+        added=datetime(2010, 1, 2),
+        last_updated=datetime(2010, 1, 3),
+    )
+
+
+@pytest.mark.parametrize(
+    'when',
+    [nothing_changes, feed_changes, feed_updated_is_newer, second_entry_is_added],
+)
+def test_entry_is_not_updated(reader, parser, when):
+    feed, old_one = prepare_feed_and_entry(reader)
+    reader.update_feeds()
+
+    reader._now = lambda: datetime(2010, 1, 3)
+    when(reader)
+    reader.update_feeds()
+
+    assert reader.get_entry(old_one) == old_one.as_entry(
+        feed=ANY,
+        added=datetime(2010, 1, 2),
+        last_updated=datetime(2010, 1, 2),
+    )
+
+
+def test_entry_is_not_updated_past_update_limit(reader, parser):
+    feed = prepare_feed(reader)
+
+    for i in range(1, 31):
+        reader._now = lambda: datetime(2010, 1, i)
+        one = parser.entry(1, 1, datetime(2010, 1, 1), title=f"one #{i}")
+        reader.update_feeds()
+
+    assert reader.get_entry(one).title == "one #25"
+
+
+# END: should (not) update
+
+
+@pytest.mark.noscheduled
+def test_not_modified(reader, parser, update_feed):
+    """A feed should not be updated if it was not modified."""
+
+    feed = parser.feed(1, datetime(2010, 1, 1), title='old')
+    reader.add_feed(feed)
+    reader._now = lambda: datetime(2010, 1, 1)
+    update_feed(reader, feed)
+
+    parser.feed(1, datetime(2010, 1, 2), title='new')
+    parser.entry(1, 1, datetime(2010, 1, 2))
+
+    reader._parser.not_modified()
+    reader._now = lambda: datetime(2010, 1, 2)
+
+    # shouldn't raise an exception
+    update_feed(reader, feed)
+
+    actual_feed = reader.get_feed(feed)
+    assert actual_feed.last_updated == datetime(2010, 1, 1)
+    assert actual_feed.last_retrieved == datetime(2010, 1, 2)
+    assert set(reader.get_entries()) == set()
+
+
+# BEGIN: edge cases
+
+
+@pytest.mark.slow
+@pytest.mark.noscheduled
+def test_parser_doesnt_block_storage(db_path, make_reader, parser):
+    reader = make_reader(db_path)
+
+    feed = parser.feed(1)
+    entry = parser.entry(1, 1)
+    reader.add_feed(feed)
+
+    reader.update_feeds()
+
+    for i in range(2, 4):
+        reader.add_feed(parser.feed(i))
+
+    parser.retrieve = Blocking(parser.retrieve)
+
+    def target():
+        with reader:
+            reader.update_feeds()
+
+    t = threading.Thread(target=target)
+    t.start()
+
+    with parser.retrieve:
+        # shouldn't raise an exception
+        reader.mark_entry_as_read((feed.url, entry.id))
+
+    t.join()
 
 
 def test_duplicate_ids(reader, parser):
@@ -153,6 +424,9 @@ def test_entry_deleted_during_update(monkeypatch, db_path, make_reader, parser):
     assert before_efu.first_updated == after_efu.first_updated
     assert before_efu.first_updated_epoch == after_efu.first_updated_epoch
     assert before_efu.recent_sort == after_efu.recent_sort
+
+
+# END: edge cases
 
 
 def test_updates_enabled_scheduled(reader, parser):
