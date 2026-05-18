4abd5a5d7a420deaab081d11af5a57322638d30c
lemon24 <damian.adrian24@gmail.com>
2026-05-18 20:12:22 +0300

    Use structlog in ._update.hooks. #147

--- src/reader/_update/hooks.py
+++ src/reader/_update/hooks.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 
-import logging
 import time
 from collections import defaultdict
 from collections.abc import Callable
 from typing import Any
 
+from .._logging import get_logger
 from ..exceptions import SingleUpdateHookError
 from ..exceptions import UpdateHookError
 from ..exceptions import UpdateHookErrorGroup
 
-log = logging.getLogger('reader.update.hooks')
+logger = get_logger('reader.update.hooks')
 
 
 class UpdateHooks:
@@ -25,6 +25,8 @@ class UpdateHooks:
         *args: Any,
         return_exceptions: bool = False,
     ) -> list[SingleUpdateHookError]:
+        log = logger.bind(when=when, **log_resource_id(resource_id))
+
         rv = []
         for hook in self.hooks[when]:
             start = time.monotonic()
@@ -42,13 +44,11 @@ class UpdateHooks:
                     name = hook.__module__ + ':' + hook.__qualname__
                 except AttributeError:
                     name = repr(hook)
-                log.debug(
-                    "ran %s hook %s for %r in %f seconds",
-                    when,
-                    name,
-                    resource_id,
-                    end - start,
-                )
+
+                timing = round(end - start, 3)
+                log_method = log.debug if timing < 1 else log.warning
+                log_method('hook_timing', hook=name, time=timing)
+
         return rv
 
     def group(self, message: str) -> _UpdateHookErrorGrouper:
@@ -76,7 +76,7 @@ class _UpdateHookErrorGrouper:
         # TODO: test error deduping; also, the logic may not be correct?
         if limit and dedupe_key not in self.seen_dedupe_keys:  # pragma: no cover
             if len(self.seen_dedupe_keys) >= limit:
-                log.error("too many hook errors; discarding exception", exc_info=exc)
+                logger.error("too many hook errors, discarding exception", exc_info=exc)
                 return
             self.seen_dedupe_keys.add(dedupe_key)
         self.exceptions.append(exc)
@@ -93,3 +93,13 @@ class _UpdateHookErrorGrouper:
         if isinstance(exc, UpdateHookErrorGroup):
             self.add(exc)
         self.close()
+
+
+def log_resource_id(resource_id: tuple[str, ...] | None) -> dict[str, str]:
+    rv: dict[str, str] = {}
+    if not resource_id:
+        return rv
+    rv['feed'] = resource_id[0]
+    if len(resource_id) >= 2:
+        rv['entry'] = resource_id[1]
+    return rv
